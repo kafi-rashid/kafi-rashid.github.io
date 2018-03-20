7eb514e3f1bccf543736d8a135dcf3b0f6dfb053
Kafi Rashid <kafi.rashid@gmail.com>
2018-03-20 15:40:41 +0600

    Init

---
+++ js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function(){
+	$.ajax({
+	    type: "GET",
+	    url: "https://api.github.com/users/kafi-rashid/repos?access_token=ed8cd7b35b09f5909ffdacd92ef2f415da2893c2",
+	    dataType: "json",
+	    success: function(result) {
+	        for(i in result) {
+	        	if (result[i].default_branch == "gh-pages") {
+	            	$('#git').append("<p><a href='https://kafi-rashid.github.io/"+ result[i].name +"' target='_blank'>" + result[i].name + "</a></p>");
+	            }
+	            else {
+	            	$('#git-2').append("<p><a href='" + result[i].svn_url + "' target='_blank'>" + result[i].name + "</a></p>");
+	            }
+	        }
+	    }
+	});
+	$('#sky').append('<div id="stars"></div>');
+	$(window).on("load resize", function(e) {
+		var stars = $('#stars');
+		$('.star').remove();
+		var vp_w = stars.width();
+		var vp_h = stars.height();
+		for (var i = 0; i < 300; i++) {
+			stars.append("<div class='star' style='left: "+Math.floor((Math.random() * vp_w) + 1)+"px; top: "+Math.floor((Math.random() * vp_h) + 1)+"px'></div>");
+		}
+		setInterval(function() {
+			for(var i = 0; i < 300; i++) {
+	            $('.star:nth-of-type('+Math.floor(Math.random() * 1) + i+')').animate({
+	                'opacity': Math.random() * 1 - (1 * 0.1)
+	            }, 500);
+	        }
+		}, 750)
+	});
+});
