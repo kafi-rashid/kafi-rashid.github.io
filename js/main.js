1b242e1915c404ab8aa309d9b8422a800fd83421
Kafi Rashid <kafi.rashid@gmail.com>
2018-03-21 17:01:46 +0600

    Fix

--- js/main.js
+++ js/main.js
@@ -9,28 +9,18 @@ $(document).ready(function(){
 	            	$('#pages').append("<p><a class='tips' href='https://kafi-rashid.github.io/"+ result[i].name +"' data-toggle='tooltip' data-placement='left' title='" + result[i].description + "'>" + result[i].name + "</a></p>");
 					$('[data-toggle="tooltip"]').tooltip(); 
 	            }
-	            else if (result[i].name == "kafi-rashid.github.io") {
-	            	
-	            }
-	            else {
-	            	$('#repos').append("<p><a class='tips' href='" + result[i].svn_url + "' data-toggle='tooltip' data-placement='left' title='" + result[i].description + "'>" + result[i].name + "</a></p>");
-	            }
+	            else if (result[i].name == "kafi-rashid.github.io") { }
+	            else $('#repos').append("<p><a class='tips' href='" + result[i].svn_url + "' data-toggle='tooltip' data-placement='left' title='" + result[i].description + "'>" + result[i].name + "</a></p>");
 	        }
 	    },
 	    error: function() {
-            $('#pages').append("API rate limit exceeded");
-            $('#repos').append("API rate limit exceeded");
+            $('#pages, #repos').append("API rate limit exceeded");
 	    }
-	});
-	$(document).click(function(e) {
-		$('#stars').prepend("<div class='star' style='left: "+e.pageX+"px; top: "+e.pageY+"px; opacity: 1'></div>");
 	});
 	$('#sky').append('<div id="stars"></div>');
 	$(window).on("load resize", function(e) {
-		var stars = $('#stars');
+		var stars = $('#stars'), vp_w = stars.width(), vp_h = stars.height();
 		$('.star').remove();
-		var vp_w = stars.width();
-		var vp_h = stars.height();
 		for (var i = 0; i < ((vp_w * vp_h) / 4000); i++) {
 			stars.append("<div class='star' style='left: "+Math.floor((Math.random() * vp_w) + 1)+"px; top: "+Math.floor((Math.random() * vp_h) + 1)+"px'></div>");
 		}
@@ -42,5 +32,8 @@ $(document).ready(function(){
 	        }
 		}, 1000);
 	});
-	$('[data-toggle="tooltip"]').tooltip(); 
+	$('[data-toggle="tooltip"]').tooltip();
+	$(document).click(function(e) {
+		$('#stars').prepend("<div class='star' style='left: "+e.pageX+"px; top: "+e.pageY+"px; opacity: 1'></div>");
+	});
 });
