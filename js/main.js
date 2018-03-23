00a1da2f3c79d6791490224e93971e930b09c47f
Kafi Rashid <kafi.rashid@gmail.com>
2018-03-23 12:14:05 +0600

    Pepe

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	var stars = $('#sky');
+	function joljol() {
+		$('.star').remove();
+		for (var i = 0; i < ((stars.width() * stars.height()) / 4000); i++) {
+			stars.append("<div class='star' style='left: " + Math.floor((Math.random() * stars.width()) + 1) + "px; top: " + Math.floor((Math.random() * stars.height()) + 1) + "px'></div>");
+		}
+	}
+	$(window).on("load resize", function(e) {
+		joljol();
+		setInterval(function() {
+			for(var i = 0; i < ((stars.width() * stars.height()) / 4000); i++) {
+	            $('.star:nth-of-type(' + Math.floor(Math.random() * 1) + i + ')').animate({
+	                'opacity': Math.random() * 1 - (1 * 0.1)
+	            }, 500);
+	        }
+		}, 1000);
+	});
+	$(document).click(function(e) {
+		stars.prepend("<div class='star' style='left: " + e.pageX + "px; top: " + e.pageY + "px; opacity: 1'></div>");
+	});
 	$.ajax({
 	    type: "GET",
 	    url: "https://api.github.com/users/kafi-rashid/repos?per_page=100",
@@ -15,23 +35,5 @@ $(document).ready(function(){
 	            $('#pages, #repos').append(result[i].message);
 	        }
 	    }
-	});
-	$('#sky').append('<div id="stars"></div>');
-	$(window).on("load resize", function(e) {
-		var stars = $('#stars'), vp_w = stars.width(), vp_h = stars.height();
-		$('.star').remove();
-		for (var i = 0; i < ((vp_w * vp_h) / 4000); i++) {
-			stars.append("<div class='star' style='left: "+Math.floor((Math.random() * vp_w) + 1)+"px; top: "+Math.floor((Math.random() * vp_h) + 1)+"px'></div>");
-		}
-		setInterval(function() {
-			for(var i = 0; i < ((vp_w * vp_h) / 4000); i++) {
-	            $('.star:nth-of-type('+Math.floor(Math.random() * 1) + i+')').animate({
-	                'opacity': Math.random() * 1 - (1 * 0.1)
-	            }, 500);
-	        }
-		}, 1000);
-	});
-	$(document).click(function(e) {
-		$('#stars').prepend("<div class='star' style='left: "+e.pageX+"px; top: "+e.pageY+"px; opacity: 1'></div>");
 	});
 });
